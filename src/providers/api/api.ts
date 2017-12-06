5518aa010c3f01b33b90a39b6ab4989e39caa956
Unknown <drazx974@gmail.com>
2017-12-06 14:31:04 +0100

    API HTTP Request for login, signup, profiles, forgotten password

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -10,6 +10,9 @@ import { Injectable } from '@angular/core';
 
 const BASE_URL = "http://familink.cleverapps.io/";
 const LOGIN = "public/login";
+const SIGNUP = "public/sign-in?contactsLength=0";
+const PROFILES = "public/profiles";
+const FORGOTTEN_PASSWORD = "public/forgot-password";
 
 @Injectable()
 export class ApiProvider {
@@ -18,9 +21,34 @@ export class ApiProvider {
     console.log('Hello ApiProvider Provider');
   }
 
+
+  // Authentications HTTP Requests
+
   login(body: {phone: String, password: String}) {
     console.log('API-PROVIDER', 'login');
-    return this.http.post(`${BASE_URL}${LOGIN}`, body).subscribe();
+    return this.http.post(`${BASE_URL}${LOGIN}`, body);
+  }
+
+  signup(body: {
+    "phone": String,
+    "password": String,
+    "firstName": String,
+    "lastName": String,
+    "email": String,
+    "profile": String // SENIOR, FAMILLE, MEDECIN
+  }) {
+    console.log('API-PROVIDER', 'signup');
+    return this.http.post(`${BASE_URL}${SIGNUP}`, body);
+  }
+
+  getProfiles() {
+    console.log('API-PROVIDER', 'profiles');
+    return this.http.get(`${BASE_URL}${PROFILES}`);
+  }
+
+  forgottenPassword(body: {phone: String}) {
+    console.log('API-PROVIDER', 'forgottenPassword');
+    return this.http.post(`${BASE_URL}${FORGOTTEN_PASSWORD}`, body);
   }
 
 }
