8f11e942025bedf43a6b719127198b43f46ad370
Thibault Goudouneix <nilmanduil@gmail.com>
2017-12-07 16:14:23 +0100

    Linked login form to AuthProvider

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, ViewController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SignUpPage } from '../sign-up/sign-up';
+import { AuthProvider } from '../../providers/auth/auth';
 
 /**
  * Generated class for the LoginPage page.
@@ -21,10 +22,10 @@ export class LoginPage {
 
 
   constructor(public viewCtrl: ViewController, public navCtrl: NavController,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder, private auth: AuthProvider) {
     this.loginForm = this.formBuilder.group({
-      phone: ['', Validators.compose([Validators.pattern('^[0-9]{10}$'), Validators.required])],
-      password: ['', Validators.compose([Validators.pattern('^[0-9]{4,}$'), Validators.required])],
+      phone: ['', Validators.compose([Validators.required, Validators.pattern('^[0-9]{10}$')])],
+      password: ['', Validators.compose([Validators.required, Validators.pattern('^[0-9]{4,}$')])],
       remember: [false],
     });
   }
@@ -34,23 +35,20 @@ export class LoginPage {
   }
 
   doLogin() {
-    console.log('Phone : ' + this.loginForm.value.phone
-      + (this.loginForm.value.phone.valid ? ' OK' : ' KO !'));
-    console.log('Password : ' + this.loginForm.value.password
-      + (this.loginForm.value.password.valid ? ' OK' : ' KO !'));
+    console.log('Phone : ' + this.loginForm.value.phone);
+    console.log('Password : ' + this.loginForm.value.password);
 
     if (this.loginForm.valid) {
-      this.viewCtrl.dismiss().then();
-    } 
+      this.auth.login(this.loginForm.value.phone, this.loginForm.value.password)
+        .then((token) => {
+          console.log('RESULT', token);
+        })
+        .catch((httpErrorResponse) => {
+          console.log('ERROR', httpErrorResponse.error.message);
+        });
 
-    // Sample code for auth provider
-    /*this.auth.login('0600000042', '0000')
-      .then((token) => {
-        console.log('RESULT', token);
-      })
-      .catch((httpErrorResponse) => {
-        console.log('ERROR', httpErrorResponse.error.message);
-      });*/
+      this.viewCtrl.dismiss().then();
+    }
   }
 
   openSignUpPage() {
