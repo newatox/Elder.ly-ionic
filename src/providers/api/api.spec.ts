eb12381074aaa1e5cd4dc1f6e97f8135441ed388
Cocatrix <cocatrix@laposte.net>
2017-12-07 13:36:37 +0100

    TSLint fix

--- src/providers/api/api.spec.ts
+++ src/providers/api/api.spec.ts
@@ -1,19 +1,19 @@
 import { ApiProvider } from './api';
-import {TestBed, async } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 describe('Api Provider: Testing Web Services', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      providers: [ApiProvider]
+      providers: [ApiProvider],
     });
   });
 
   it('Token length should be 156', async(() => {
-    let apiP = TestBed.get(ApiProvider);
-    apiP.login({phone: "0600000042", password: "0000"}).subscribe(jsonToken => {
-      expect(jsonToken["token"].length).toBe(156)
-    })
+    const apiP = TestBed.get(ApiProvider);
+    apiP.login({ phone: '0600000042', password: '0000' }).subscribe((jsonToken) => {
+      expect(jsonToken['token'].length).toBe(156);
+    });
   }));
 });
