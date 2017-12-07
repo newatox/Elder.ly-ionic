b3e3da424ee8b28ab6530649c042a92153b6ba64
Cocatrix <cocatrix@laposte.net>
2017-12-07 11:43:20 +0100

    Something working, before refactoring

--- src/providers/api/api.spec.ts
+++ src/providers/api/api.spec.ts
@@ -1,38 +1,35 @@
 import { ApiProvider } from './api';
-import { TestBed, getTestBed, inject } from '@angular/core/testing';
+import {TestBed, getTestBed, async, inject} from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 describe('Api Provider: Testing Web Services', () => {
-    let injector: TestBed;
+    //let injector: TestBed;
     //let apiProvider: ApiProvider;
-    let httpMock: HttpTestingController;
+    //let httpMock: HttpTestingController;
 
     beforeEach(() => {
       TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule],
+        imports: [HttpClientModule],
         providers: [ApiProvider]
       });
-      injector = getTestBed();
+      //injector = getTestBed();
       //apiProvider = injector.get(ApiProvider);
-      httpMock = injector.get(HttpTestingController);
-    });
-    /*
-    afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
-      httpMock.verify();
-    }));
-    */
-    it('should get a token', () => {
-      inject([ApiProvider], (apiProvider) =>
-      apiProvider.login({phone: "0600000042", password: "0000"}).subscribe(jsonToken => {
-        console.log('test jsonToken : ', jsonToken);
-        expect(jsonToken.hasOwnProperty("token"))
-      }));
+      //httpMock = injector.get(HttpTestingController);
     });
 
-  it('should have a token of known length 156', () => {
-    inject([ApiProvider], (apiProvider) =>
-      apiProvider.login({phone: "0600000042", password: "0000"}).subscribe(jsonToken => {
-        expect(jsonToken["token"].toString.length).toBe(156)
-      }));
-  });
+    it('should get a foo', inject([ApiProvider], (apiProvider) =>
+        apiProvider.login({phone: "0600000042", password: "0000"}).subscribe(jsonToken => {
+          console.log('test jsonToken 1 : ', jsonToken);
+          expect('a').toEqual('a')
+        }))
+    );
+
+    it('should get a foo', async(() => {
+      let apiP = TestBed.get(ApiProvider);
+      apiP.login({phone: "0600000042", password: "0000"}).subscribe(jsonToken => {
+        console.log('test jsonToken 2 : ', jsonToken);
+        expect('a').toEqual('b')
+      })
+    }));
 });
