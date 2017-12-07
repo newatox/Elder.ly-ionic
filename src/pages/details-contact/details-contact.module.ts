5439bd7b5510926ab39135b3bdc38efc9dc188e2
Cocatrix <cocatrix@laposte.net>
2017-12-07 16:54:25 +0100

    DetailView: image name segment

--- src/pages/details-contact/details-contact.module.ts
+++ src/pages/details-contact/details-contact.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { DetailsContactPage } from './details-contact';
+import Contact from '../../models/Contact';
 
 @NgModule({
   declarations: [
--- src/pages/details-contact/details-contact.ts
+++ src/pages/details-contact/details-contact.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActionSheetController, IonicPage,
   NavController, Platform } from 'ionic-angular';
 import { AddEditContactPage } from '../add-edit-contact/add-edit-contact';
+import Contact from '../../models/Contact';
 
 /**
  * Generated class for the DetailsContactPage page.
@@ -16,8 +17,23 @@ import { AddEditContactPage } from '../add-edit-contact/add-edit-contact';
   templateUrl: 'details-contact.html',
 })
 export class DetailsContactPage {
+  public contact: Contact;
+
   constructor(public navCtrl: NavController,
               public alertCtrl: ActionSheetController, public platform: Platform) {
+    this.contact = new Contact(
+      '0600000042',
+      'Jean-Patrick',
+      'Dupont',
+      'aaaa@aaa.com',
+      'SENIOR',
+      false,
+      false,
+      false,
+      0,
+      '',
+      'https://www.shareicon.net/download/2016/07/05/791214_man_512x512.png',
+    );
   }
 
   present() {
