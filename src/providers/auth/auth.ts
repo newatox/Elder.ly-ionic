cbd24a9310b7a38cb70cb75882f2e0c4e2d61cab
Thibault Goudouneix <nilmanduil@gmail.com>
2017-12-14 14:09:47 +0100

    Reconnection alert appear only once

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -18,6 +18,7 @@ export class AuthProvider {
 
   private auth: User;
   private token: any;
+  private reconnectionAlertVisible = false;
 
   constructor(public http: HttpClient,
               private api: ApiProvider,
@@ -128,49 +129,57 @@ export class AuthProvider {
 
   invalidToken() {
     if (this.network.isOnline()) {
-      const alert = this.alertCtrl.create({
-        title: this.translate.instant('INVALID_TOKEN_TITLE'),
-        subTitle: this.translate.instant('INVALID_TOKEN_SUBTITLE'),
-        enableBackdropDismiss: false,
-        inputs: [
-          {
-            name: 'password',
-            type: 'password',
-            placeholder: this.translate.instant('PASSWORD'),
-            min: 4,
-            max: 4,
-          },
-        ],
-        buttons: [
-          {
-            text: this.translate.instant('CANCEL_LABEL'),
-            role: 'cancel',
-            handler: () => {
-              console.log('Cancel pressed in Invalid token box');
+      if (!this.reconnectionAlertVisible) {
+        this.reconnectionAlertVisible = true;
+        const alert = this.alertCtrl.create({
+          title: this.translate.instant('INVALID_TOKEN_TITLE'),
+          subTitle: this.translate.instant('INVALID_TOKEN_SUBTITLE'),
+          enableBackdropDismiss: false,
+          inputs: [
+            {
+              name: 'password',
+              type: 'password',
+              placeholder: this.translate.instant('PASSWORD'),
+              min: 4,
+              max: 4,
             },
-          },
-          {
-            text: this.translate.instant('OK_LABEL'),
-            handler: (data) => {
-              if (/^[0-9]{4,}$/.test(data.password)) {
-                console.log('Login from invalid token alert');
-                this.login(this.auth.phone, data.password)
-                  .then((data) => {
-                    console.log(data);
-                  })
-                  .catch((error) => {
-                    console.log(error.message);
-                    this.invalidToken();
-                  });
-              } else {
-                console.log('Invalid password from invalid token alert');
-                this.invalidToken();
-              }
+          ],
+          buttons: [
+            {
+              text: this.translate.instant('CANCEL_LABEL'),
+              role: 'cancel',
+              handler: () => {
+                console.log('Cancel pressed in Invalid token box');
+              },
             },
-          },
-        ],
-      });
-      alert.present().then();
+            {
+              text: this.translate.instant('OK_LABEL'),
+              handler: (data) => {
+                if (/^[0-9]{4,}$/.test(data.password)) {
+                  console.log('Login from invalid token alert');
+                  this.login(this.auth.phone, data.password)
+                    .then((data) => {
+                      console.log(data);
+                    })
+                    .catch((error) => {
+                      console.log(error.message);
+                      this.invalidToken();
+                    });
+                } else {
+                  console.log('Invalid password from invalid token alert');
+                  this.invalidToken();
+                }
+              },
+            },
+          ],
+        });
+        alert.onWillDismiss(() => {
+          this.reconnectionAlertVisible = false;
+        });
+        alert.present().then();
+      } else {
+        console.log('Reconnection alert already visible');
+      }
     } else {
       this.network.showMessage();
     }
